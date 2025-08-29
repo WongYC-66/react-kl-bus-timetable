c952561ba1f9e45c5b48f129909112af49452c4a
WongYC-66 <yengchien@hotmail.com>
2025-08-29 15:07:33 +0800

    configure: github io CI/CD

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  base: "/react-kl-bus-timetable/",
+
   plugins: [
     react(),
     tailwindcss(),
